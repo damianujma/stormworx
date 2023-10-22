6b43c0bae478081441b588bd6e07433bc293198b
damianujma <damianujma.kontakt@gmail.com>
2023-10-22 16:05:23 +0200

    Send notification

---
+++ runApp.sh
@@ -0,0 +1,2 @@
+sleep 60
+./gradlew run
---
+++ sendNotification.sh
@@ -0,0 +1,5 @@
+curl --location --request POST 'localhost:8080/alarm-conditions/sendPredictions' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "email": "damianujma.kontakt@gmail.com"
+}'
