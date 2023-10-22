b28757b40b5753a677561df7cb6a09c067ad49b1
damianujma <damianujma.kontakt@gmail.com>
2023-10-22 16:40:51 +0200

    Add secret email

--- sendNotification.sh
+++ sendNotification.sh
@@ -3,3 +3,8 @@ curl --location --request POST 'localhost:8080/alarm-conditions/sendPredictions'
 --data-raw '{
     "email": "damianujma.kontakt@gmail.com"
 }'
+curl --location --request POST 'localhost:8080/alarm-conditions/sendPredictions' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "email": "$SECRET_EMAIL"
+}'
